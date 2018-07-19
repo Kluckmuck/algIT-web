90a8976a24f543ad0a00544e7f14b753daf60b1f
Kluckmuck <kluckmucki@gmail.com>
2018-07-19 10:24:50 +0200

    binded name input with ngModel

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgsRevealConfig } from 'ng-scrollreveal';
 
+import { Message } from  './message';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,7 +17,7 @@ export class AppComponent {
   }
   // form variables
   submitted = false;
-  
+
   onSubmit() { this.submitted = true; }
 
   sendMessage() {
